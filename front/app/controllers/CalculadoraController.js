69e4bacdf28de582d2aa01dd9075b81bf23ec250
Debora Souza <debora.souzalima22@gmail.com>
2024-11-21 18:49:55 -0300

    Incluindo a função calcularTudo para aparecer os resultados das 4 operações. Incluindo a função que limpa os campos e o resultado.

--- front/app/controllers/CalculadoraController.js
+++ front/app/controllers/CalculadoraController.js
@@ -6,14 +6,51 @@ angular.module('meuApp')
     $scope.valores = {
         primeiroNumero : 0,
         segundoNumero : 0,
-        resultado: 0
+        resultado: 0,
     };
 
-
+    /* Funções individuais */
     $scope.somar = function () {
         $scope.valores.resultado = $scope.valores.primeiroNumero + $scope.valores.segundoNumero;
-        console.log($scope.valores.resultado);
-        
+        console.log($scope.valores.resultado); 
+    };
+    $scope.subtrair = function () {
+        $scope.valores.resultado = $scope.valores.primeiroNumero - $scope.valores.segundoNumero;
+        console.log($scope.valores.resultado);    
+    };
+    $scope.multiplicar = function () {
+        $scope.valores.resultado = $scope.valores.primeiroNumero * $scope.valores.segundoNumero;
+        console.log($scope.valores.resultado);    
+    };
+    $scope.dividir = function () {
+        if ($scope.valores.segundoNumero == 0) {
+            $scope.valores.resultado = "Não é possível dividir por 0!"
+        }else{
+            $scope.valores.resultado = $scope.valores.primeiroNumero / $scope.valores.segundoNumero;
+            console.log($scope.valores.resultado);    
+
+        }
     };
 
+    /* Função geral (calcula todas as operações)*/
+    $scope.carcularTudo = function(){
+        soma = $scope.valores.primeiroNumero + $scope.valores.segundoNumero;
+        subtracao = $scope.valores.primeiroNumero - $scope.valores.segundoNumero;
+        multiplicacao = $scope.valores.primeiroNumero * $scope.valores.segundoNumero;
+        if($scope.valores.segundoNumero == 0){
+            divisao = "Não é possível dividir por 0!"
+        }else{
+           divisao = $scope.valores.primeiroNumero / $scope.valores.segundoNumero;
+        };
+        $scope.valores.resultado = `Soma: ${soma}, Subtração: ${subtracao}, Multiplicação: ${multiplicacao}, Divisão: ${divisao}`
+    };
+
+    /* Função que limpa os campos */
+    $scope.limpar = function(){
+        $scope.valores = {
+            primeiroNumero : 0,
+            segundoNumero : 0,
+            resultado: 0,
+        };
+    }
 });
