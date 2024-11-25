431ee42fc4d23335a41c449fb0c3696baf35dec8
Debora Souza <debora.souzalima22@gmail.com>
2024-11-25 12:06:55 -0300

    Incluindo a função que recebe os dados, calcula a idade e limpa os campos no Controller. No HTML incluindo a tabela e adicionando o objto pessoas, na data de nascimento utilizando o pipe | date: 'dd/MM/yyyy' para formartar para o padrão brasileiro.

--- front/app/controllers/CalculadoraIdadeController.js
+++ front/app/controllers/CalculadoraIdadeController.js
@@ -4,13 +4,46 @@ angular.module('meuApp')
     console.log("Calduladora de idade funcionando")
 
     $scope.informacoes = {
+        nome: '',
         dataDeNascimento: '',
         idadeCalculada: ''
     }
 
-    $scope.calcularIdade = function(){
-        console.log($scope.informacoes.dataDeNascimento);
+    // Criando o Objeto pessoas[]
+    $scope.pessoas = []
+
+    $scope.calcularIdade = function(){ 
+        // Pegando o nome do campo input
+        nomeInserido = $scope.informacoes.nome;
+
+        // Pegando a data de nascimento e o dia atual
+        dataNascimento = new Date($scope.informacoes.dataDeNascimento);
+        hoje = new Date();
         
+        // Pegando o ano, mes e dia
+        anos = hoje.getFullYear() - dataNascimento.getFullYear();
+        meses = hoje.getMonth() - dataNascimento.getMonth();
+        dias = hoje.getDate() - dataNascimento.getDate();
+
+        // Ajustando se o mês ou dia ainda não ocorreram
+        if(dias < 0){
+            meses--;
+            dias += new Date(hoje.getFullYear(), hoje.getMonth(), 0).getDate(); // dias do mês anterior
+        }
+        if(meses < 0){
+            anos --;
+            meses += 12;
+        }
+        $scope.informacoes.idadeCalculada = `${nomeInserido} tem ${anos} anos, ${meses} meses e ${dias} dias.`;
+
+        $scope.pessoas.push(angular.copy($scope.informacoes));
+
+        // Limpando os campos
+        $scope.informacoes = {
+            nome: '',
+            dataDeNascimento: '',
+            idadeCalculada: ''
+        }
     }
 
 });
