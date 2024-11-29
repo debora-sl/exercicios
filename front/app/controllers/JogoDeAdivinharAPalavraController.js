36203bf79d512ccdd70803691ac644a55431c92a
Debora Souza <debora.souzalima22@gmail.com>
2024-11-28 21:50:33 -0300

    Incluindo mais variaveis e funções de testes.

--- front/app/controllers/JogoDeAdivinharAPalavraController.js
+++ front/app/controllers/JogoDeAdivinharAPalavraController.js
@@ -4,21 +4,35 @@ angular.module('meuApp')
     console.log("Jogo funcionando")
 
     $scope.jogo = {
+        quantidadeTentativa: 0,
+        quantidadeLetrasIguais: 0,
+        jogoIniciado: 0,
         palavraTentada: '',
-        palavraAleatoria: ''
     }
+
+    $scope.palavraAleatoria= '',
+
+    palavras = ['abacaxi', 'banana', 'laranja', 'uva', 'melancia', 'pêssego', 'ameixa', 'manga', 'kiwi' ]
 
     $scope.iniciarJogo = function(){
         console.log('Botão clicado!');
-        
-        
+
         
     }
     
     $scope.verificarPalavra = function(){
         console.log('Botão verificar palavra clicado!');
 
+    }
 
+    $scope.reiniciarOJogo = function(){
+        console.log('Botão reiniciar o jogo, clicado!');
+        
+    }
+
+    calcularLetrasIguais = function(){
+        palavra1 = $scope.palavraAleatoria;
+        palavra2 = $scope.jogo.palavraTentada;
     }
 
 });
