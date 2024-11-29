65ff593f6c6d9dcd7b50329ad388b00dc9f5a1cc
Debora Souza <debora.souzalima22@gmail.com>
2024-11-29 13:16:40 -0300

    Incluindo mais variaveis e funções para mais testes.

--- front/app/controllers/JogoDeAdivinharAPalavraController.js
+++ front/app/controllers/JogoDeAdivinharAPalavraController.js
@@ -4,35 +4,54 @@ angular.module('meuApp')
     console.log("Jogo funcionando")
 
     $scope.jogo = {
-        quantidadeTentativa: 0,
+        palavraTentada: '',
+        quantidadeTentativas: 0,
         quantidadeLetrasIguais: 0,
         jogoIniciado: 0,
-        palavraTentada: '',
+        mensagem: ''
     }
 
-    $scope.palavraAleatoria= '',
+    palavras = [
+        'abacaxi', 'banana', 'laranja', 'morango', 'coco',
+        'melancia', 'pessego', 'uva', 'pera',
+        'ameixa', 'kiwi', 'manga', 'framboesa', 'limao',
+        'tangerina', 'cereja', 'amora', 'maracuja', 'goiaba'
+    ];
 
-    palavras = ['abacaxi', 'banana', 'laranja', 'uva', 'melancia', 'pêssego', 'ameixa', 'manga', 'kiwi' ]
+    $palavraAleatoria= '';
 
     $scope.iniciarJogo = function(){
-        console.log('Botão clicado!');
-
+        console.log('botão iniciar o jogo clicado');
+        palavraAleatoria = palavras[Math.floor(Math.random() * palavras.length)];
+        
+        console.log(palavraAleatoria);
         
     }
-    
+
     $scope.verificarPalavra = function(){
-        console.log('Botão verificar palavra clicado!');
-
-    }
-
-    $scope.reiniciarOJogo = function(){
-        console.log('Botão reiniciar o jogo, clicado!');
+        console.log('botão verificar Palavra clicado');
+        $scope.jogo.palavraTentada++;
+        $scope.jogo.quantidadeLetrasIguais = calcularLetrasIguais();
+        console.log($scope.jogo.quantidadeLetrasIguais);
+        
         
     }
 
     calcularLetrasIguais = function(){
-        palavra1 = $scope.palavraAleatoria;
+        palavra1 = palavraAleatoria;
         palavra2 = $scope.jogo.palavraTentada;
+
+        contador = 0;
+        comprimento = Math.min(palavra1.length, palavra2.length);
+
+        for (let i = 0; i < comprimento; i++) {
+            if (palavra1[i] === palavra2[i]) {
+                contador++;
+            }
+        }
+
+        return contador;
     }
 
+    
 });
