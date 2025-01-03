c9491fcef8311b264babed0e7de773c6b4d285fc
Debora Souza <debora.souzalima22@gmail.com>
2025-01-02 21:42:16 -0300

    Ajustando no HomeController e no header.html para que o nome do usuário seja exibido. No botão deslogar incluido o direcionamento para deslogar.

--- front/app/controllers/HomeController.js
+++ front/app/controllers/HomeController.js
@@ -1,5 +1,17 @@
 // $scope é para comunicação entre JS e HTML
 angular.module('meuApp')
-.controller('HomeController', function($scope) {
+.controller('HomeController', function($scope, $state) {
     $scope.saudacaoHome = 'Olá, você está na tela Home!'
+
+    // trazendo dados do bd
+    $token = localStorage.getItem('token');
+    $scope.usuario = JSON.parse(localStorage.getItem('usuario'));
+    
+    console.log($token);
+
+    // verificando se usurário está logado, se não é direcionado para o login
+    if($token == null){
+        $state.go('login');
+    }
+
 });
